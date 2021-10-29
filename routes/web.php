873c3405e7b1802707a29045290b7b3aba20cfa9
GUO CHIH-LING <s3a832098@student.ncut.edu.tw>
2021-10-29 22:29:42 +0800

    建立HelloController，設定web.php中的Route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\HelloController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -20,5 +20,7 @@ Route::get('/', function () {
     Route::get('home', function () {
         return view('home.index');
     });
+
+    Route::get('hello/{name?}',[HelloController::class,'index']) -> name('hello.index.');
 
 ?>
