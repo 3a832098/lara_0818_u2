0f1d7506d0f9b58c6fba8d33d1637ee4cec3469b
GUO CHIH-LING <s3a832098@student.ncut.edu.tw>
2021-10-29 22:21:52 +0800

    編輯navigation.blade.php，新增ul li項目符號

---
+++ resources/views/layouts/partials/navigation.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE HTML>
+<head>
+    <title></title>
+</head>
+<body>
+
+    <ul>
+        <li>
+            <a href="#">Item 1</a>
+        </li>
+        <li>
+            <a href="#">Item 2</a>
+        </li>
+
+    </ul>
+
+
+</body>
+</html>
