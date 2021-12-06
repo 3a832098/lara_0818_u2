d1500f06b183aa33dcd73b9a7a8d6e85cd00f9ab
GUO CHIH-LING <3a832098@gm.student.ncut.edu.tw>
2021-12-06 19:31:36 +0800

    修改練習3-4 設定HelloController，將參數傳遞至view

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -7,7 +7,12 @@ use Illuminate\Http\Request;
 class HelloController extends Controller
 {
     //
+    public function index($name='Everybody')
+    {
+        $data = ['name' => $name];
+        return view ('hello.index',$data);
 
+    }
 }
 
 
